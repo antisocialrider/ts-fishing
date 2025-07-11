91c77d63847c9397a417075141a1594459c4b96e
antisocialrider <tyler.beard.2024@gmail.com>
2025-07-11 16:19:42 +0100

    tweak

--- server/main.lua
+++ server/main.lua
@@ -39,16 +39,3 @@ AddEventHandler('onResourceStart', function(resourceName)
         DebugPrint('Fishing server script started.')
     end
 end)
-
-if Config.Items.create then
-    if not exports['qb-core'] or type(exports['qb-core'].AddItem) ~= 'function' then
-        print('^1[ts-fishing] Error: exports[\'qb-core\']:AddItem is not available. Ensure qb-core is fully loaded and updated.^7')
-        return
-    end
-    for _, itemData in ipairs(Config.Items.list) do
-        exports['qb-core']:AddItem(itemData.name, itemData)
-        print(string.format('^2[ts-fishing] Registered new item via AddItem: %s (%s)^7', itemData.label, itemData.name))
-    end
-else
-    print('^3[ts-fishing] Item creation is disabled in config. To enable, set Config.Items.create to true.^7')
-end
